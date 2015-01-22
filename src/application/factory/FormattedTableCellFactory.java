f64d9dcc6f3df5f6c3183fb24ac930ff1b116286
ak24448 <ak24448@LAP313-002.win.edag.de>
2015-01-22 15:49:10 +0100

    dragndrop

--- src/application/factory/FormattedTableCellFactory.java
+++ src/application/factory/FormattedTableCellFactory.java
@@ -6,8 +6,12 @@ import javafx.scene.control.TableColumn;
 import javafx.util.Callback;
 import application.MainApplication;
 import application.model.Lesson;
+import application.model.LessonTimeInformation;
 import application.model.TimePeriod;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.Dragboard;
 import javafx.scene.input.MouseButton;
+import javafx.scene.input.TransferMode;
 
 public class FormattedTableCellFactory<S,T> implements Callback<TableColumn<TimePeriod, ObservableList<Lesson>>, TableCell<TimePeriod, ObservableList<Lesson>>> {
 
@@ -17,8 +21,10 @@ public class FormattedTableCellFactory<S,T> implements Callback<TableColumn<Time
 	public TableCell<TimePeriod, ObservableList<Lesson>> call(TableColumn<TimePeriod, ObservableList<Lesson>> param) {
 		// TODO Auto-generated method stub
 		return new TableCell<TimePeriod, ObservableList<Lesson>>() {
+			String tempStyle = "";
 	        @Override
 	        protected void updateItem(ObservableList<Lesson> item, boolean empty) {
+	        	
 	            super.updateItem(item, empty);    
 	            this.setOnMouseClicked((event)->{
 	            	if(event.getClickCount() == 2){
@@ -38,6 +44,57 @@ public class FormattedTableCellFactory<S,T> implements Callback<TableColumn<Time
 	            this.setOnMouseEntered((event)->{
 	            });
 	            this.setOnMouseExited((event)->{
+	            });
+	            this.setOnDragDetected((event)->{
+	            	if(item != null && item.get(0).isEmpty() == false){
+	            		String s = "";
+	            		for(Lesson l: item){
+	            			if(l!= null){
+	            				s += l.getId()+",";
+	            			}
+	            		}
+	            		Dragboard dragboard = startDragAndDrop(TransferMode.COPY);
+		                ClipboardContent content = new ClipboardContent();
+		                content.putString(s);
+		                dragboard.setContent(content);
+		                event.consume();
+	            	}
+	            	
+	            });
+	            setOnDragOver(event -> {
+	                if (event.getGestureSource() != this && event.getDragboard().hasString()) {
+	                	if(item.get(0)!= null){
+	                		event.acceptTransferModes(TransferMode.COPY);
+	                	}
+	                    
+	                }
+	                event.consume();
+	            });
+	            setOnDragEntered(event -> {
+	                if (event.getGestureSource() != this && event.getDragboard().hasString()) {
+	                    setOpacity(0.3);
+	                    tempStyle = getStyle();
+	                    setStyle("-fx-border-color: black");
+	                }
+	            });
+	 
+	            setOnDragExited(event -> {
+	                if (event.getGestureSource() != this && event.getDragboard().hasString()) {
+	                	setStyle(tempStyle);
+	                    setOpacity(1);
+	                }
+	            });
+	            setOnDragDropped(event ->{
+	            	Dragboard db = event.getDragboard();
+	            	boolean success = false;
+	            	if(db.hasString()){
+	            		String[] ids = db.getString().split(",");
+	            		LessonTimeInformation newTime = item.get(0).getTimeInformation();
+	            		//TODO: select lesson anhand id save in new time information
+	            		success = true;
+	            	}
+	            	event.setDropCompleted(success);
+	            	event.consume();
 	            });
 	            String text = "";
 	            if(item != null && item.get(0).isEmpty() == false){
